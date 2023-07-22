895a2ddd43f7fb445e2bb0549f05cf10e431dbe8
AsaadIqbalKhan <106610630+AsaadIqbalKhan@users.noreply.github.com>
2023-07-23 03:21:48 +0530

    Add files via upload
    uploading fresh project

---
+++ project masai/project2.py
@@ -0,0 +1,14 @@
+# from sketchpy import library
+# image = library.rdj()
+# image.draw()
+
+from sketchpy import canvas
+obj = canvas.sketch_from_image(r"C:\Users\It Solution\Desktop\project masai\peaceful.jpg")
+obj.draw()
+
+# from sketchpy import canvas
+# obj = canvas.sketch_from_image(r"C:\Users\It Solution\Desktop\project masai\peaceful.jpg")
+# obj.draw(threshold=120)
+
+
+
---
+++ project masai/tempCodeRunnerFile.py
@@ -0,0 +1,3 @@
+from sketchpy import canvas
+obj = canvas.sketch_from_image(r"C:\Users\It Solution\Desktop\project masai\future mars.jpg")
+obj.draw()
